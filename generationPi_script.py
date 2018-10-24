651fc202c2b4dca1c75f1c7e8946d5b50d7e7e1b
Наталья <natakhatina@gmail.com>
2018-10-24 21:03:12 +0300

    ДЗ 3

--- generationPi_script.py
+++ generationPi_script.py
@@ -1,31 +1,21 @@
-
 import math
-# x=range(91)
-# x=math.radians(x)
 
-# L=[math.cos(x*math.pi/180) for x in range(91)]
-# print(L)
-# print(L[90])
+def foo(n):
+    for x in range(n):
+        x=x * math.pi / 180
+        y=x*math.sin(x)
+        yield y
 
-k=2
-def foo(k):
-    x=(k+0.5)*math.pi
-    y=x*math.sin(x)
-    yield [x, y]
+Spisok=[y for y in foo(91)]
+print(Spisok[90])
 
-# print(foo(1))
+def bar(x):
+    if math.sin(x)*math.cos(x)>0:
+        yield math.sin(x)
+    else:
+        yield math.cos(x)
 
-def bar(k):
-    x=[]
-    for i in range(0,k+1):
-        el=(i+0.5)*math.pi
-        yield el
-
-
-L=[x*math.sin(x) for x in bar(k)]
-print(L)
-
-X=[(i+0.5)*math.pi for i in range(0,k)]
-Y=[(i+0.5)*math.pi*math.sin((i+0.5)*math.pi) for i in range(0,k)]
-print(X)
-print(Y)
+x=-60
+x=x * math.pi / 180
+res=bar(x)
+print(next(res))
